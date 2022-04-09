60a01bcd49dd2aa7d63dc651f28c48266fc75372
ruasvmv <maksim.suvorov@raiffeisen.ru>
2022-04-09 19:05:45 +0300

    added html templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,24 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def hello_world():
-    return 'Hello, World!'
+    user = {'username': 'msuvorov7'}
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+
+    return render_template('index.html', user=user, posts=posts)
